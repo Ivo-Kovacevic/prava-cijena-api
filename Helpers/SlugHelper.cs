b8fac259f3cbc7a8e1adaac0bb5e0717a96b9af5
Ivo <131997439+Ivo-Kovacevic@users.noreply.github.com>
2025-03-03 19:47:55 +0100

    update slug generator

--- Helpers/SlugHelper.cs
+++ Helpers/SlugHelper.cs
@@ -6,10 +6,17 @@ public class SlugHelper
 {
     public static string GenerateSlug(string name)
     {
+        // Normalize string to separate diacritics from characters
         var slug = name.Normalize(System.Text.NormalizationForm.FormD);
-        slug = Regex.Replace(slug, @"[\p{Mn}]", "");
-        slug = slug.Trim().ToLower().Replace(" ", "-");
+        
+        // Remove any diacritical marks and percentage sign
+        slug = Regex.Replace(slug, @"[\p{Mn}%]", "");
+        
+        // Replace any sequence of periods, commas or dashes with single dash
+        slug = Regex.Replace(slug, @"[., ]+|-{2,}", "-");
+        
+        slug = slug.TrimStart('-');
 
-        return slug;
+        return slug.ToLower();
     }
 }
